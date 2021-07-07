a999378ab9ec445a19a21c60e12b6b83ee2e9816
Jonathan Wheeler <jwheeler18@my.scccd.edu>
2021-07-06 19:16:40 -0700

    Calendar App Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calendar from './components/Calendar';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const jsx = (
+    <Calendar />
+)
 
-export default App;
+ReactDOM.render(jsx, document.getElementById('app'))
---
+++ src/components/Calendar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import moment from 'moment';
+
+const Calendar = () => (
+    <div>
+        <h1>Calendar App</h1>
+        <p>My first calendar component.</p>
+    </div>
+);
+
+export default Calendar;
