99044640592c175aa6de6ff1bc4a12c0784a3855
Jonathan Wheeler <jwheeler18@my.scccd.edu>
2021-07-07 15:02:53 -0700

    setup jest and enzyme

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import moment from 'moment';
 
+const currentDate = moment();
+
 const Calendar = () => (
     <div>
         <h1>Calendar App</h1>
+        <h2>{currentDate.format('MMMM')}</h2>
         <p>My first calendar component.</p>
     </div>
 );
