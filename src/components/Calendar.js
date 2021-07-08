6d20900e741a7f0d73a3df6797734defce9f7d19
Jonathan Wheeler <jwheeler18@my.scccd.edu>
2021-07-08 00:23:31 -0700

    setup calendar squares with days

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,48 +1,56 @@
 import React from 'react';
 import moment from 'moment';
+import CalendarDay from './CalendarDay';
 
-const currentDate = moment();
+const currentDate = moment().add(2, 'months');
 
-const getCalendarDays = (calendarDate) => {
-    const daysInMonth = calendarDate.daysInMonth();
-    const monthFirstDay = moment(calendarDate).startOf('month');
-    const monthFirstDayDOW = monthFirstDay.day();
-    const monthLastDay = moment(calendarDate).endOf('month');
-    const monthLastDayDOW = monthLastDay.day();
-    const prevMonthLastDay = moment(calendarDate).subtract(1, 'months').endOf('month');
-    const nextMonthFirstDay = moment(calendarDate).add(1, 'months').startOf('month');
-    const calendarDays = [];
+export default class Calendar extends React.Component {
+    getCalendarDays = (calendarDate) => {
+        const daysInMonth = calendarDate.daysInMonth();
+        const monthFirstDay = moment(calendarDate).startOf('month');
+        const monthFirstDayDOW = monthFirstDay.day();
+        const monthLastDay = moment(calendarDate).endOf('month');
+        const monthLastDayDOW = monthLastDay.day();
+        const prevMonthLastDay = moment(calendarDate).subtract(1, 'months').endOf('month');
+        const nextMonthFirstDay = moment(calendarDate).add(1, 'months').startOf('month');
+        const calendarDays = [];
+        
+        for(let i=prevMonthLastDay.date() - monthFirstDayDOW + 1; i<=prevMonthLastDay.date(); i++) {
+            calendarDays.push({
+                type: 'pre',
+                date: moment(prevMonthLastDay).date(i).startOf('day')
+            });
+        }
+        for(let i=monthFirstDay.date(); i<=monthLastDay.date(); i++) {
+            calendarDays.push({
+                type: 'current',
+                date: moment(calendarDate).date(i).startOf('day')
+            });
+        }
+        for(let i=nextMonthFirstDay.date(); i<=(nextMonthFirstDay.date() + (6-monthLastDayDOW) - 1); i++) {
+            calendarDays.push({
+                type: 'next',
+                date: moment(nextMonthFirstDay).date(i).startOf('day')
+            });
+        }
     
-    for(let i=prevMonthLastDay.date() - monthFirstDayDOW + 1; i<=prevMonthLastDay.date(); i++) {
-        calendarDays.push({
-            type: 'pre',
-            date: moment(prevMonthLastDay).date(i).startOf('day')
-        });
+        return calendarDays
     }
-    for(let i=monthFirstDay.date(); i<=monthLastDay.date(); i++) {
-        calendarDays.push({
-            type: 'current',
-            date: moment(calendarDate).date(i).startOf('day')
-        });
+    render() {
+        return (
+            <div>
+                <h1>Calendar App</h1>
+                <h2>{currentDate.format('MMMM')}</h2>
+                <div className="month-display">
+                    {this.getCalendarDays(currentDate).map((day) => (
+                        <CalendarDay 
+                            date={day.date}
+                            key={day.date.valueOf()}
+                            type={day.type}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
     }
-    for(let i=nextMonthFirstDay.date(); i<=(nextMonthFirstDay.date() + (6-monthLastDayDOW) - 1); i++) {
-        calendarDays.push({
-            type: 'next',
-            date: moment(nextMonthFirstDay).date(i).startOf('day')
-        });
-    }
-
-    return calendarDays
 }
-
-const monthDays = getCalendarDays(currentDate);
-console.log(monthDays)
-const Calendar = () => (
-    <div>
-        <h1>Calendar App</h1>
-        <h2>{currentDate.format('MMMM')}</h2>
-        {monthDays.map((day) => (<p>{day.date.format()}</p>))}
-    </div>
-);
-
-export default Calendar;
--- src/components/CalendarDay.js
+++ src/components/CalendarDay.js
@@ -1,1 +1,21 @@
 import React from 'react';
+import moment from 'moment';
+
+export default class CalendarDay extends React.Component {
+    selectDate = () => {
+        console.log(this.props.date);
+    }
+    render() {
+        return (
+            <div 
+                className={this.props.type==='current' ? "month-day" : "month-day pre-next"}
+                title={this.props.date.format("MMM Do")}
+                onClick={this.selectDate}
+            >
+                <div className="content">
+                    <div className="day-num">{this.props.date.format("D")}</div>
+                </div>                
+            </div>
+        );
+    }
+}
