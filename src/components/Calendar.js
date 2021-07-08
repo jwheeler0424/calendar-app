62aba850e77759b3501f63510a38ca1c7c2d897a
Jonathan Wheeler <jwheeler18@my.scccd.edu>
2021-07-07 19:27:01 -0700

    create days

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,36 +1,47 @@
 import React from 'react';
 import moment from 'moment';
 
-const currentDate = moment().add(2, 'months');
-const daysInMonth = currentDate.daysInMonth();
-const monthFirstDay = moment(currentDate).startOf('month');
-const monthFirstDayDOW = monthFirstDay.day();
-const monthLastDay = moment(currentDate).endOf('month');
-const monthLastDayDOW = monthLastDay.day();
-const prevMonthLastDay = moment(currentDate).subtract(1, 'months').endOf('month');
-const nextMonthFirstDay = moment(currentDate).add(1, 'months').startOf('month');
-const prevDays = [];
-const calendarDays = [];
-const nextDays = [];
-for(let i=prevMonthLastDay.date() - monthFirstDayDOW + 1; i<=prevMonthLastDay.date(); i++) {
-    prevDays.push(i);
-}
-for(let i=monthFirstDay.date(); i<=monthLastDay.date(); i++) {
-    calendarDays.push(i);
-}
-for(let i=nextMonthFirstDay.date(); i<=(nextMonthFirstDay.date() + (6-monthLastDayDOW) - 1); i++) {
-    nextDays.push(i);
+const currentDate = moment();
+
+const getCalendarDays = (month) => {
+    const daysInMonth = month.daysInMonth();
+    const monthFirstDay = moment(month).startOf('month');
+    const monthFirstDayDOW = monthFirstDay.day();
+    const monthLastDay = moment(month).endOf('month');
+    const monthLastDayDOW = monthLastDay.day();
+    const prevMonthLastDay = moment(month).subtract(1, 'months').endOf('month');
+    const nextMonthFirstDay = moment(month).add(1, 'months').startOf('month');
+    const calendarDays = [];
+
+    for(let i=prevMonthLastDay.date() - monthFirstDayDOW + 1; i<=prevMonthLastDay.date(); i++) {
+        calendarDays.push({
+            type: 'pre',
+            date: i
+        });
+    }
+    for(let i=monthFirstDay.date(); i<=monthLastDay.date(); i++) {
+        calendarDays.push({
+            type: 'current',
+            date: i
+        });
+    }
+    for(let i=nextMonthFirstDay.date(); i<=(nextMonthFirstDay.date() + (6-monthLastDayDOW) - 1); i++) {
+        calendarDays.push({
+            type: 'next',
+            date: i
+        });
+    }
+
+    return calendarDays
 }
 
+const monthDays = getCalendarDays(currentDate);
+console.log(monthDays)
 const Calendar = () => (
     <div>
         <h1>Calendar App</h1>
         <h2>{currentDate.format('MMMM')}</h2>
-        <p>{prevDays.join(',')}</p>
-        <p>{calendarDays.join(',')}</p>
-        <p>{nextDays.join(',')}</p>
-        <p>{monthFirstDayDOW} - {monthFirstDay.format('dddd')}</p>
-        <p>{}</p>
+        {monthDays.map((day) => (`${day.date} `))}
     </div>
 );
 
