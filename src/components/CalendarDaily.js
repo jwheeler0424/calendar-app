f2a8c48e5a901bdbedb62ea5849b5a3c09970f88
Jonathan Wheeler <jwheeler18@my.scccd.edu>
2021-07-17 11:52:43 -0700

    complete calendar display function

--- src/components/CalendarDaily.js
+++ src/components/CalendarDaily.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
+import { NavigateBefore, NavigateNext } from '@material-ui/icons';
+import { setCurrentDate } from '../actions/views';
 import CalendarDay from './CalendarDay';
 
 export class CalendarDaily extends React.Component {
+    setDayPrev = () => {
+        const prevDay = moment(this.props.views.currentDate).subtract(1, 'days').valueOf();
+        this.props.setCurrentDate(prevDay);
+    }
+    setDayNext = () => {
+        const nextDay = moment(this.props.views.currentDate).add(1, 'days').valueOf();
+        this.props.setCurrentDate(nextDay)
+    }
     render () {
+        const prevDay = moment(this.props.views.currentDate).subtract(1, 'days');
+        const nextDay = moment(this.props.views.currentDate).add(1, 'days');
         return (
             <div className="calendar-daily__wrapper">
-                <h1>Daily</h1>
+                <button onClick={this.setDayPrev}><NavigateBefore /> {prevDay.format('MMM Do')}</button>
+                <button onClick={this.setDayNext}>{nextDay.format('MMM Do')} <NavigateNext /></button>
+                <h1>{moment(this.props.views.currentDate).format('MMMM Do, YYYY')}</h1>
             </div>
         );
     }
