69c87ace5ba9331611b8072e5133f55afc0b4604
Jonathan Wheeler <jwheeler18@my.scccd.edu>
2021-07-21 23:43:29 -0700

    connect color picker to even form

--- src/components/ColorSelector.js
+++ src/components/ColorSelector.js
@@ -15,7 +15,42 @@ const ColorSelector = (props) => {
         const title = e.target.attributes[0].value;
         const value = e.target.attributes[1].value;
         setColor({ title, value});
+        props.onColorChange(title);
     }
+    const colors = [{ 
+        title: 'banana',
+        value: 'rgb(245, 192, 58)' 
+    }, {
+        title: 'basil',
+        value: 'rgb(17, 129, 71)'
+    }, {
+        title: 'blueberry',
+        value: 'rgb(66, 78, 178)'
+    }, {
+        title: 'flamingo',
+        value: 'rgb(228, 124, 116)'
+    }, {
+        title: 'grape',
+        value: 'rgb(142, 26, 167)'
+    }, {
+        title: 'graphite',
+        value: 'rgb(97, 97, 97)'
+    }, {
+        title: 'lavendar',
+        value: 'rgb(122, 132, 201)'
+    }, {
+        title: 'peacock',
+        value: 'rgb(121, 185, 225)'
+    }, {
+        title: 'sage',
+        value: 'rgb(55, 183, 124)'
+    }, {
+        title: 'tangerine',
+        value: 'rgb(242, 81, 37)'
+    }, {
+        title: 'tomato',
+        value: 'rgb(211, 0, 6)'
+    }];
     return (
         <div className="color-selector__wrapper">
             <div className="color-selector__selected" onClick={toggleColorSelectMenu}>
@@ -24,7 +59,7 @@ const ColorSelector = (props) => {
             </div>
             <div className={'color-selector__selector' + colorMenuOpen}>
                 <div className="color-selector__select">
-                    {props.colors.map(({ title, value }) => (
+                    {colors.map(({ title, value }) => (
                             <div
                                 key={title}
                                 onClick={selectColor}
--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -21,6 +21,9 @@ const EventForm = (props) => {
     const [endDate, setEndDate] = useState(
         props.event ? props.event.endDate : moment().minute(getRoundedMinute()).startOf('minute').add(1, 'hour')
     );
+    const [color, setColor] = useState(
+        props.event ? props.event.color : 'peacock'
+    );
     const disabledSeconds = [];
     for (let i=0; i<60; i++) {
         disabledSeconds.push(i);
@@ -29,45 +32,12 @@ const EventForm = (props) => {
         const title = e.target.value;
         setTitle(title);
     }
-    
+    const onColorChange = (color) => {
+        setColor(color);
+    };
     const onSubmit = (e) => {
         e.preventDefault();
     }
-    const colors = [{ 
-            title: 'banana',
-            value: 'rgb(245, 192, 58)' 
-        }, {
-            title: 'basil',
-            value: 'rgb(17, 129, 71)'
-        }, {
-            title: 'blueberry',
-            value: 'rgb(66, 78, 178)'
-        }, {
-            title: 'flamingo',
-            value: 'rgb(228, 124, 116)'
-        }, {
-            title: 'grape',
-            value: 'rgb(142, 26, 167)'
-        }, {
-            title: 'graphite',
-            value: 'rgb(97, 97, 97)'
-        }, {
-            title: 'lavendar',
-            value: 'rgb(122, 132, 201)'
-        }, {
-            title: 'peacock',
-            value: 'rgb(121, 185, 225)'
-        }, {
-            title: 'sage',
-            value: 'rgb(55, 183, 124)'
-        }, {
-            title: 'tangerine',
-            value: 'rgb(242, 81, 37)'
-        }, {
-            title: 'tomato',
-            value: 'rgb(211, 0, 6)'
-        }
-    ];
     return (
         <form onSubmit={onSubmit}>
             <div>
@@ -118,13 +88,13 @@ const EventForm = (props) => {
             </div>
             <div>
                 <span>Event Color</span>
-                <ColorSelector colors={colors} />
+                <ColorSelector color={color} onColorChange={onColorChange} />
             </div>
             <div>
-                <input type="radio" name="event-duration" id="event-duration-hourly" value="time" checked />
-                <label for="event-duration-hourly" title="Select Hourly Duration">Time</label>
+                <input type="radio" name="event-duration" id="event-duration-hourly" value="time" defaultChecked={true} />
+                <label htmlFor="event-duration-hourly" title="Select Hourly Duration">Time</label>
                 <input type="radio" name="event-duration" id="event-duration-daily" value="day" />
-                <label for="event-duration-daily" title="Select All Day Duration">All Day</label>
+                <label htmlFor="event-duration-daily" title="Select All Day Duration">All Day</label>
             </div>
             <div>
                 <input type="text" name="event-location" placeholder="Location" title="Event Location" />
