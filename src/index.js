e921ac4d63213e13b373b04d1b8366d1f6e1c58c
Jonathan Wheeler <jwheeler18@my.scccd.edu>
2021-08-21 14:34:34 -0700

    heroku build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
-import configureStore from '/store/configureStore';
+import configureStore from './store/configureStore';
 import { firebase } from './firebase/firebase';
 import { login, logout } from './actions/auth';
 import { startPopulateEvents } from './actions/events';
